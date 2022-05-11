37d5a191919f25c6eeba1c003600f8b8f86228f6
greatAdams01 <kingifean@gmail.com>
2022-05-11 03:04:25 +0100

    Deploy scripts

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -6,7 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // 1000000000000000000000
 
 contract RewardToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("SIRINU", "SIU") {
-        _mint(msg.sender, initialSupply);
+  uint256 public s_maxSupply = 1000000000000000000000000;
+    constructor() ERC20("SIRINU", "SIU") {
+        _mint(msg.sender, s_maxSupply);
     }
 }
