b88f3e0235a39151cefcdae22fe9ab1b94f6f453
greatAdams01 <kingifean@gmail.com>
2022-05-11 03:28:51 +0100

    Added rinkeby network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,10 +2,15 @@ import "hardhat-deploy"
 import '@nomiclabs/hardhat-ethers'
 import '@typechain/hardhat'
 import { HardhatUserConfig } from 'hardhat/config'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   networks: {
@@ -14,6 +19,10 @@ const config: HardhatUserConfig = {
     },
     localhost: {
       chainId: 31337
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${PRIVATE_KEY}`]
     }
   },
   solidity: {
@@ -25,5 +34,8 @@ const config: HardhatUserConfig = {
     }
   }
 }
+// On Rinkey testnet
+// Staking contract : 0xE118636d30Ce6eAA684693065c1F10B1f26fF529
+// Reward contract : 0xb0Cc385454eC3DD52EE00353f4E9E37D79CA67E0
 
 export default config;
